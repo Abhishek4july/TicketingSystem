157ba76aea5ab3369db932e8a89792c4f0594f30
Abhishek4july <abhi0407july@gmail.com>
2025-06-16 13:27:22 +0530

    Fix CORS and cookie config

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,6 +5,16 @@ dotenv.config({
     path:'./.env'
 })
 
+import fs from "fs";
+import path from "path";
+
+const tempDir = path.join("public", "temp");
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+  console.log("✅ Created missing folder: public/temp");
+}
+
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT||8000,()=>{
